830a869e73bbcaca83719b18180af2fa11411eb3
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-07-15 18:07:27 -0600

    [object]create menu

---
+++ js/pizza.js
@@ -0,0 +1,24 @@
+const menu = {
+    spicyPig: {
+        toppings: ['pepperoni', 'cheese', 'jalapenos'],
+        price: 12.99
+    },
+    meatLover: {
+        toppings: ['pepperoni', 'cheese', 'bacon'],
+        price: 15.99
+    },
+    meatHater: {
+        toppings: ['onions', 'cheese', 'olives', 'basil'],
+        price: 9.99
+    },
+    moreCheese: {
+        toppings: ['cheese', 'cheese', 'cheese', 'more cheese'],
+        price: 10.99
+    },
+    notPizzza: {
+        toppings: ['fake cheese', 'gluten free crust', 'beyond meat'],
+        price: 16.99
+    },
+};
+
+console.log(menu.keys);  // this needs some work
